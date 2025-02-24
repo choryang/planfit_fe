e5c5b8335a0c7dde492d597d50d355763d9a90f3
choryang <okee0815@gmail.com>
2025-02-24 11:31:47 +0900

    Fix: axios base URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({baseURL: "https://planfit-fe.vercel.app"})
+const client = axios.create({baseURL: "http://localhost:5173"})
 
 export default client;
